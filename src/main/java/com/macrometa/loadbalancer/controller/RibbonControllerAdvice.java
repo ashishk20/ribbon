6781bd9f0967bc9a14cc79e57a8f63db2b8deb6a
Ashish Sen <ashish.sen@tietoevry.com>
2022-07-29 10:59:04 +0530

    custom load balancer

--- src/main/java/com/macrometa/loadbalancer/controller/RibbonControllerAdvice.java
+++ src/main/java/com/macrometa/loadbalancer/controller/RibbonControllerAdvice.java
@@ -4,13 +4,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.client.HttpClientErrorException;
 
 @ControllerAdvice
 public class RibbonControllerAdvice {
 
-    @ExceptionHandler(HttpClientErrorException.class)
+    @ExceptionHandler(Exception.class)
     public ResponseEntity<String> exception(Exception e) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("null");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 }
